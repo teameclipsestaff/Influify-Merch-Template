4a56a5fe933059daa8871ae33216d2b92fa53853
teameclipsestaff <teameclipseao@gmail.com>
2021-07-19 07:19:55 -0400

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./style.css";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 // Components
 import Header from './components/Header';
 
+// Pages
+import HomePage from './pages/HomePage';
 export default function App() {
   return (
     <>
+    <Router>
       <Header />
-      <h1>Coming Soon</h1>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
     </>
   );
 }
