123407789dc40f98fb7cadc546560f1b1c898824
teameclipsestaff <teameclipseao@gmail.com>
2021-07-19 20:11:16 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -10,20 +10,6 @@ import Footer from './components/Footer';
 // Pages
 import HomePage from './pages/HomePage';
 
-const client = new GraphQLClient({
-  uri: "https://graphql.myshopify.com/api/2020-04/graphql.json",
-  headers: {
-    "X-Shopify-Storefront-Access-Token": "dd4d4dc146542ba7763305d71d1b3d38",
-  },
-  // typeDefs: gql`
-  //   enum ProductSortKeys {
-  //     TITLE
-  //     UPDATED_AT
-  //     BEST_SELLING
-  //     PRICE
-  //   }
-  // `,
-});
 
 export default function App() {
   return (
